b6810c32b83f23bfa45c1c2dcc78937611f30b08
Julia Cruz <131014598+jjuliacp@users.noreply.github.com>
2025-03-16 14:49:28 -0500

    implement auto-click on show more btn

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -2,12 +2,39 @@ import { getItems } from './utils/wong';
 
 console.log('Estoy en contentScript 2.0')
 
-chrome.runtime.onConnect.addListener(function(port){
-    port.onMessage.addListener(async ({cmd})=>{
-        console.log(cmd)
-        if(cmd == "getItems"){
+const buttonShowMore = async (): Promise<HTMLElement | null> => {
+
+    let btn = document.querySelectorAll('.vtex-search-result-3-x-buttonShowMore')[1].firstElementChild as HTMLElement;
+    while (btn.hasAttribute('disabled')) {
+        console.log("⌛ Botón deshabilitado, esperando...");
+        await new Promise(resolve => setTimeout(resolve, 2000)); // Espera 2 segundo
+    }
+    btn.click();
+    console.log("✅ Botón habilitado, haciendo clic...");
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    return btn;
+};
+
+// bucle para hacer click en el boton show more hasta que muestre todos items
+const loadTotalItems = async () => {
+    let btn = await buttonShowMore();
+    if (btn) {
+
+        await loadTotalItems();
+
+    } else {
+        console.log("🛒 Todos los items cargados");
+    }
+};
+
+
+chrome.runtime.onConnect.addListener(function (port) {
+    port.onMessage.addListener(async ({ cmd }) => {
+        console.log("📩 Comando recibido:", cmd)
+        if (cmd == "getItems") {
+            await loadTotalItems();
             const data = getItems()
-            port.postMessage({success:true,message:"Items obtenidos",data})
+            port.postMessage({ success: true, message: "Items obtenidos", data })
         }
-    } )
+    })
 })
